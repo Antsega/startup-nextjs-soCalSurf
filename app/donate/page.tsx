edadedb0b0475c60ebce407e8e6b94750efd99ee
Antsega <astrowelding@gmail.com>
2023-06-16 13:32:40 -0700

    added mock donation page

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,17 +1,61 @@
+'use client'
+import React from 'react';
 import Breadcrumb from "@/components/Common/Breadcrumb";
-import Contact from "@/components/Contact";
 
-const ContactPage = () => {
+const DonationPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Donation Page"
+        description="Support our mission and contribute to a better world. Your generosity makes a difference."
       />
 
-      <Contact />
+      <div className="donation-container" style={donationContainerStyle}>
+        <h2 style={headingStyle}>Make a Donation</h2>
+        <p style={paragraphStyle}>
+          Your donation helps us to continue our efforts and make a positive impact. We appreciate your support!
+        </p>
+        <div style={buttonContainerStyle}>
+          <button style={donationButtonStyle} onClick={() => alert("Thank you for your donation!")}>Donate Now</button>
+        </div>
+      </div>
     </>
   );
 };
 
-export default ContactPage;
+const donationContainerStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '50px',
+  textAlign: 'center'
+};
+
+const headingStyle: React.CSSProperties = {
+  fontSize: '32px',
+  marginBottom: '20px'
+};
+
+const paragraphStyle: React.CSSProperties = {
+  fontSize: '18px',
+  marginBottom: '30px'
+};
+
+const buttonContainerStyle: React.CSSProperties = {
+  display: 'flex',
+  justifyContent: 'center'
+};
+
+const donationButtonStyle: React.CSSProperties = {
+  fontSize: '20px',
+  padding: '15px 30px',
+  backgroundColor: '#4CAF50',
+  color: 'white',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  textDecoration: 'none'
+};
+
+export default DonationPage;
